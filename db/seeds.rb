f08e9addd8a42cc6b233a5051c7b615db51d0863
Mahyar <mahyar.erfanian1998@gmail.com>
2023-12-06 15:58:08 -0500

    Add airport model and seed records to the airport table in the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Airport.delete_all
+puts "started seeding..."
+Airport.create(code:"YUL", name:"Montréal-Pierre Elliott Trudeau International Airport", city:"Montreal", country:"Canada")
+Airport.create(code: 'SFO', name: 'San Francisco International Airport', city: 'San Francisco', country: 'United States')
+Airport.create(code: 'JFK', name: 'John F. Kennedy International Airport', city: 'New York', country: 'United States')
+Airport.create(code: 'LAX', name: 'Los Angeles International Airport', city: 'Los Angeles', country: 'United States')
+Airport.create(code: 'PIT', name: 'Pittsburgh International Airport', city: 'Pittsburgh', country: 'United States')
+Airport.create(code: 'SAN', name: 'San Diego International Airport', city: 'San Diego', country: 'United States')
+Airport.create(code: 'DTW', name: 'Detroit Metropolitan Airport', city: 'Detroit', country: 'United States')
+Airport.create(code: 'ORD', name: 'O\'hare International Airport', city: 'Chicago', country: 'United States')
+Airport.create(code: 'SLC', name: 'Salt Lake City International Airport', city: 'Salt Lake City', country: 'United States')
+Airport.create(code: 'DFW', name: 'Dallas / Fort Worth International Airport', city: 'Dallas', country: 'United States')
+Airport.create(code: 'SEA', name: 'Seattle-Tacoma International Airport', city: 'Seattle', country: 'United States')
+Airport.create(code: 'BOS', name: 'Logan International Airport', city: 'Boston', country: 'United States')
+puts "seeding was successful!"
