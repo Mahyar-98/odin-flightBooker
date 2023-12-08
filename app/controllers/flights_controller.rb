fe1e10d97516171e44a63876fcc7162c4d40eae8
Mahyar <mahyar.erfanian1998@gmail.com>
2023-12-08 17:06:09 -0500

    Create the search form for flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,6 @@
 class FlightsController < ApplicationController
+    def index
+        @airport_options = Airport.all.map { |ap| [ap.name, ap.id] }
+        @date_options = Flight.all.map { |flight| [flight.date, flight.date]}.uniq
+    end
 end
